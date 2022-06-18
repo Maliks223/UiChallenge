4cececc500ba7b4fefcc78d86d02489f1cc7f686
malik <malikakkawi223@gmail.com>
2022-06-18 17:01:29 +0300

    REsponsive

--- src/Components/NavBar/navBar.js
+++ src/Components/NavBar/navBar.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "../NavBar/navBar.css";
 import Logo from "../recources/RollsRoyceLogo.png";
-import { useInView } from 'react-intersection-observer';
-import clsx from 'clsx';
+import { useInView } from "react-intersection-observer";
+import clsx from "clsx";
 
 const NavBar = () => {
   const { ref, inView } = useInView({
     threshold: 0,
   });
 
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div ref={ref} className={clsx("navBar", inView && 'navbar-inview')}>
+    <div ref={ref} className={clsx("navBar", inView && "navbar-inview")}>
       <div className="navBarLeft">
         <div className="navBarTitle">Rolls Royce</div>
         <div>
@@ -19,9 +21,28 @@ const NavBar = () => {
       </div>
 
       <div className="navBarRight">
-        <a href="#cars-section">Cars</a>
-        <a href="#about-section">About Us</a>
-        <a href="#contact-section">Contact Us</a>
+        <a className="link" href="#cars-section">Cars</a>
+        <a className="link" href="#about-section">About Us</a>
+        <a className="link" href="#contact-section">Contact Us</a>
+      </div>
+      <button
+        className="burger-menu"
+        onClick={() => setToggle((prev) => !prev)}
+      >
+        <i class="fa fa-bars icon"></i>
+      </button>
+
+      <div className={clsx("mobile-menu", toggle && "toggle")}>
+          <i onClick={() => setToggle((prev) => !prev)} class="fa fa-close close-icon"></i>
+        <a className="link-1" onClick={() => setToggle((prev) => !prev)} href="#cars-section">
+          Cars
+        </a>
+        <a className="link-2" onClick={() => setToggle((prev) => !prev)} href="#about-section">
+          About Us
+        </a>
+        <a className="link-3" onClick={() => setToggle((prev) => !prev)} href="#contact-section">
+          Contact Us
+        </a>
       </div>
     </div>
   );
--- src/Components/card/card.js
+++ src/Components/card/card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../card/card.css";
 import { useInView } from "react-intersection-observer";
 import clsx from "clsx";
@@ -7,13 +7,20 @@ const Card = ({ image, title, carInfo }) => {
   const { ref, inView } = useInView({
     threshold: 0,
   });
+
+  // const [showResults, setShowResults] = useState(false);
+  // const onClick = () => setShowResults((prev) => !prev);
+
   return (
     <div ref={ref} className={clsx("card", inView && `card-inview`)}>
       <div className="cardImage">
         <img src={image}></img>
+        {/* <button className="cardBtn" onClick={onClick}>
+          {showResults ? "Less Info" : "More info "}
+        </button> */}
       </div>
-      <div>
-        <div>{title}</div>
+      <div className="cardInner">
+        <div className="cardTitle">{title}</div>
         <div className="hidden-text">{carInfo}</div>
       </div>
     </div>
